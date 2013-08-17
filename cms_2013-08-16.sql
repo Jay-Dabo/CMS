ff9628b85e4d62aff37856e07fd9bf0b10338b69
Ryan Dawkins <dawkinssu@gmail.com>
2013-08-16 23:52:44 -0500

    Created CMS database export for the basic database design.

---
+++ cms_2013-08-16.sql
@@ -0,0 +1,69 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.29)
+# Database: cms
+# Generation Time: 2013-08-17 04:50:12 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table address
+# ------------------------------------------------------------
+
+CREATE TABLE `address` (
+  `addressid` int(16) unsigned NOT NULL AUTO_INCREMENT,
+  `address1` varchar(128) DEFAULT NULL,
+  `address2` varchar(128) DEFAULT NULL,
+  `city` varchar(64) DEFAULT NULL,
+  `state` varchar(64) DEFAULT NULL,
+  `zipcode` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`addressid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table home
+# ------------------------------------------------------------
+
+CREATE TABLE `home` (
+  `homeid` int(16) unsigned NOT NULL AUTO_INCREMENT,
+  `addressid` int(16) DEFAULT NULL,
+  PRIMARY KEY (`homeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table people
+# ------------------------------------------------------------
+
+CREATE TABLE `people` (
+  `peopleid` int(16) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(64) DEFAULT NULL,
+  `middleName` varchar(64) DEFAULT NULL,
+  `lastName` varchar(64) DEFAULT NULL,
+  `homeid` int(16) DEFAULT NULL,
+  PRIMARY KEY (`peopleid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
